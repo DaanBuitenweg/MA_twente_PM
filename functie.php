24008ee45c9311bd8b35d5628adf3f83dfe2b019
DaanBuitenweg <566171@edu.rocmn.nl>
2022-03-25 11:00:20 +0100

    added a functions file

---
+++ functie.php
@@ -0,0 +1,32 @@
+<?php
+function geefinfoHtml($info)
+{
+
+    $html = '';
+    foreach ($info as $key => $rij) {
+        $vraag          = $rij["vraag"];
+        $antwoord       = $rij["antwoord"];
+        $categorie       = $rij["categorie"];
+
+        $html .= "<br>$vraag        $antwoord       $categorie";
+    }
+
+    return $html;
+}
+
+
+function toonAlleinfo()
+{
+    $sql        = "SELECT vraag, antwoord, categorie
+                    FROM faq
+                    ORDER BY vraag";
+                    
+    $pdo = maakPDO();
+
+    $result = $pdo->query($sql);
+    $info = $result->fetchAll();
+
+    echo geefinfoHtml($info);
+}
+
+?>
